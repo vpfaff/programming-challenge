932ad2321d0f2ef45cfa537071cabf1878ff6bda
vpfaff <verena.pfaff@uni-ulm.de>
2020-11-01 16:45:10 +0100

    Add test case for data handler class

---
+++ src/test/java/de/exxcellent/challenge/DataHandlerTest.java
@@ -0,0 +1,27 @@
+package de.exxcellent.challenge;
+
+import de.exxcellent.challenge.util.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+public class DataHandlerTest {
+
+    private final String TEST_FILE_URL = "test.csv";
+    private final String[] TEST_FILE_HEADERS = {"Identifier", "MaxVal", "MinVal"};
+
+    private final CSVReader csvReader = new CSVReader(TEST_FILE_URL,
+            TEST_FILE_HEADERS[0],
+            TEST_FILE_HEADERS[1],
+            TEST_FILE_HEADERS[2]);
+
+    private final DataHandler dataHandler = new DataHandler(csvReader);
+
+    @Test
+    public void getSmallestSpreadTest() throws IOException {
+        String successLabel = "2";
+        assertEquals("identifier must be 2", successLabel, dataHandler.getSmallestSpreadIdentifier());
+    }
+}
